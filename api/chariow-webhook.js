98b4be61a9ea56ee5b7cf5d6b0ee976cfa9637a8
aloyi229 <aloyiathanase@gmail.com>
2026-05-20 09:14:11 +0200

    Afficher données webhook

--- api/chariow-webhook.js
+++ api/chariow-webhook.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   try {
     const data = req.body;
 
-    console.log("Paiement reçu :", data);
+    console.log(JSON.stringify(data, null, 2));
 
     return res.status(200).json({
       success: true,
