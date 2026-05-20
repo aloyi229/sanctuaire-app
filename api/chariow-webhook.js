a28eabbcf463f1938b6142e0660cff95c8d733b0
aloyi229 <aloyiathanase@gmail.com>
2026-05-20 09:58:08 +0200

    Ajout système premium

--- api/chariow-webhook.js
+++ api/chariow-webhook.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
@@ -9,6 +12,28 @@ export default async function handler(req, res) {
     const data = req.body;
 
     console.log(JSON.stringify(data, null, 2));
+
+    const email = data.customer?.email;
+    const status = data.sale?.status;
+
+    if (status === "completed" && email) {
+
+      const filePath = path.join(process.cwd(), "premium-users.json");
+
+      let users = [];
+
+      if (fs.existsSync(filePath)) {
+        users = JSON.parse(fs.readFileSync(filePath));
+      }
+
+      if (!users.includes(email)) {
+        users.push(email);
+
+        fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+      }
+
+      console.log("Utilisateur premium ajouté :", email);
+    }
 
     return res.status(200).json({
       success: true,
